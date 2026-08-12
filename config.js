94819c784bbaca4aee1129323cdd30773ad52429
h-tanaka-ume <h.tanaka@umegaoka.org>
2026-08-12 23:09:54 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.INVENTORY_CONFIG = {
   supabaseUrl: "https://wpoghiifdyossvpbutvw.supabase.co/rest/v1/",
-  supabaseApIKey: "sb_publishable_2pMlmzXc4uJtruz1GVS_ow_1i2bSx4m",
+  supabaseAnonKey: "sb_publishable_2pMlmzXc4uJtruz1GVS_ow_1i2bSx4m",
   enableRealtime: true
 };
